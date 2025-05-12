61130b2fe8dcf2797f926aac9d35ab7948da7c50
Santiago Restrepo <santiago.restrepo@criticalmass.com>
2025-05-12 06:14:32 -0500

    remove eslint

--- init.lua
+++ init.lua
@@ -646,15 +646,6 @@ require('lazy').setup({
           end,
         },
       }
-
-      require('lspconfig').eslint.setup {
-        on_attach = function(client, bufnr)
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            buffer = bufnr,
-            command = 'EslintFixAll',
-          })
-        end,
-      }
     end,
   },
 
